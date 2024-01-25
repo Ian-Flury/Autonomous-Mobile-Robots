db3050e1414ad8259004b54666652b6b37a0377f
Ian-Flury <ianflury123@gmail.com>
2024-01-25 10:35:09 -0800

    redoing project setup

--- Lab_3/main.c
+++ Lab_3/main.c
@@ -1,12 +1,11 @@
 #include "msp.h"
+#include "Clock.h"
+#include "Motor.h"
 
 
-/**
- * main.c
- */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	Clock_Init48MHz();
-
+	
 }
