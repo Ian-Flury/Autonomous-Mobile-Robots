b4ad56298831a9635cc7e740233c0b526614cbd4
joeymacauley02 <joeymacauley02@gmail.com>
2024-02-06 23:10:21 -0800

    psuedo code, added motor and pwm

--- Lab_4/main.c
+++ Lab_4/main.c
@@ -3,6 +3,8 @@
 #include "BumpInt.h"
 #include "Clock.h"
 #include "RobotLights.h"
+#include "Motor.h"
+#include "PWM.h"
 
 void handle_collision(uint8_t ISR_data)
 {
@@ -19,11 +21,34 @@ void main(void)
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	Clock_Init48MHz();
 	MvtLED_Init();
-
+	Motor_Init();
 	BumpInt_Init(&handle_collision);
+	uint8_t bump;
 
 	while (1)
 	{
+	    MotorForward(); // 33 percent duty cycle
 	    Clock_Delay1ms(1000);
+	    bump = ;
+	    // if bump
+	    if (bump...)
+	    {
+            // stop
+            MotorStop();
+
+            // backup for 400 ms
+            MotorBackward();
+
+            // if bump left
+            if (bump > ) {
+                // turn right
+                MotorRight();
+            }
+            // if bump right
+            else if (bump < ) {
+                // turn left
+                MotorLeft();
+            }
+	    }
 	}
 }
