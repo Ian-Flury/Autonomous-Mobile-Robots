739d8c6ff90c2fa880f188a6a13120f0c4aeb6c0
Ian-Flury <ianflury123@gmail.com>
2024-02-27 10:43:33 -0800

    dist measurement done

---
+++ Lab_6_Part_A/main.c
@@ -0,0 +1,52 @@
+#include "msp.h"
+#include "Clock.h"
+#include "ADC14.h"
+
+#define NUM_OF_SAMPLES 30
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	// initialization functions
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	Clock_Init48MHz();
+	ADC0_InitSWTriggerCh17_14_16();
+
+	//  A17,  A16,   A14
+	uint32_t left, right, center;
+
+
+	uint32_t left_buf[NUM_OF_SAMPLES];
+	uint32_t center_buf[NUM_OF_SAMPLES];
+	uint32_t right_buf[NUM_OF_SAMPLES];
+
+	float left_avg, center_avg, right_avg;
+
+	while (1)
+	{
+		// collect the samples
+		int i;
+		for (i = 0; i < NUM_OF_SAMPLES; i++)
+		{
+			ADC_In17_14_16(&left, &center, &right);
+			left_buf[i] = left;
+			center_buf[i] = center;
+			right_buf[i] = right;
+		}
+
+		// compute the average of the samples
+		for (i = 0; i < NUM_OF_SAMPLES; i++)
+		{
+			left_avg += (float)left_buf[i];
+			center_avg += (float)center_buf[i];
+			right_avg += (float)right_buf[i];
+		}
+		left_avg = left_avg / NUM_OF_SAMPLES;
+		center_avg = center_avg / NUM_OF_SAMPLES;
+		right_avg = right_avg / NUM_OF_SAMPLES;
+
+		Clock_Delay1ms(1000);
+	}
+}
